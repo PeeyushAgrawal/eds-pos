fa47ce9b47fa2fd8ed613c43add03ee81a0db341
peeyush.agrawal@publicissapient.com <peeyush.ag0212@gmail.com>
2026-04-27 11:04:05 +0530

    Refactor product list component: update IDs and style options, remove unused model

--- blocks/product-list/product-list.js
+++ blocks/product-list/product-list.js
@@ -1,34 +1,33 @@
-// // Function to initialize the component and make the HTTP request
-// async function initProductListComponents() {
-//   // Select all div elements with the class 'product-list-container'
-//   const divs = document.querySelectorAll('.product-list-container');
+// Function to initialize the component and make the HTTP request
+async function initProductListComponents() {
+  // Select all div elements with the class 'product-list-container'
+  const divs = document.querySelectorAll('.product-list-container');
 
-//   // Iterate over each div and perform the necessary operations
-//   divs.forEach((div) => {
-//     function createProductList(products) {
-//       let productListHTML = '';
-//       products.forEach((product) => {
-//         productListHTML += `
-//                           <div class="product-item">
-//                              <a href="${`plp?product=${product.name}`}"
-// aria-label="View details for ${product.name}">
-//                               <img src="${'https://placehold.jp/400x400.png'}" alt="${product.altText}">
-//                                <h2>${product.name}</h2>
-//                               <div class="product-description">${product.description}</div>
-//                               </a>
-//                           </div>
-//                       `;
-//       });
-//       div.querySelector('.product-list').innerHTML += productListHTML;
-//     }
-//     const { apivalue } = div.dataset;
-//     // Extract data attributes from the div
-//     const fetchAndDisplayProducts = async () => {
-//       const resp = await fetch(apivalue);
-//       const jsono = await resp.json();
-//       createProductList(jsono.data);
-//     };
-//     // fetchAndDisplayProducts();
-//   });
-// }
-// initProductListComponents();
+  // Iterate over each div and perform the necessary operations
+  divs.forEach((div) => {
+    function createProductList(products) {
+      let productListHTML = '';
+      products.forEach((product) => {
+        productListHTML += `
+                          <div class="product-item">
+                             <a href="${`plp?product=${product.name}`}" aria-label="View details for ${product.name}">
+                              <img src="${'https://placehold.jp/400x400.png'}" alt="${product.altText}">
+                               <h2>${product.name}</h2>
+                              <div class="product-description">${product.description}</div>
+                              </a>
+                          </div>
+                      `;
+      });
+      div.querySelector('.product-list').innerHTML += productListHTML;
+    }
+    const { apivalue } = div.dataset;
+    // Extract data attributes from the div
+    const fetchAndDisplayProducts = async () => {
+      const resp = await fetch(apivalue);
+      const jsono = await resp.json();
+      createProductList(jsono.data);
+    };
+    fetchAndDisplayProducts();
+  });
+}
+initProductListComponents();
